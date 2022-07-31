4c432dfa006c659bb6da5b622eb7e9638dcfbc35
Vitalii Psl <vitalii.psl.main@gmail.com>
2022-07-31 21:32:05 +0300

    Add User entity, which implement UserDetails interface, and Role enum

---
+++ src/main/java/com/example/springjwtauthentication/model/entity/Role.java
@@ -0,0 +1,5 @@
+package com.example.springjwtauthentication.model.entity;
+
+public enum Role {
+    CLIENT, ADMIN
+}
---
+++ src/main/java/com/example/springjwtauthentication/model/entity/User.java
@@ -0,0 +1,65 @@
+package com.example.springjwtauthentication.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "person")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String fullName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.toString()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
