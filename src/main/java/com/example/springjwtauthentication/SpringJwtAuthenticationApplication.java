e0cc094882f5782a556d9ec3917466fd96a45de8
Vitalii Psl <vitalii.psl.main@gmail.com>
2022-07-31 21:44:20 +0300

    Save default admin user on application startup

--- src/main/java/com/example/springjwtauthentication/SpringJwtAuthenticationApplication.java
+++ src/main/java/com/example/springjwtauthentication/SpringJwtAuthenticationApplication.java
@@ -1,13 +1,46 @@
 package com.example.springjwtauthentication;
 
+import com.example.springjwtauthentication.model.entity.Role;
+import com.example.springjwtauthentication.model.entity.User;
+import com.example.springjwtauthentication.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class SpringJwtAuthenticationApplication {
+    @Value("${admin.email}")
+    private String adminEmail;
+
+    @Value("${admin.password}")
+    private String adminPassword;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringJwtAuthenticationApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(UserService userService, PasswordEncoder encoder) {
+        return args -> {
+            User admin = getAdmin(encoder);
+
+            Optional<User> possibleUser = userService.getUser(admin.getEmail());
+            if (possibleUser.isEmpty()) {
+                userService.saveUser(admin);
+            }
+        };
+    }
+
+    private User getAdmin(PasswordEncoder encoder) {
+        return User.builder()
+                .email(adminEmail)
+                .password(encoder.encode(adminPassword))
+                .role(Role.ADMIN)
+                .build();
+    }
 }
